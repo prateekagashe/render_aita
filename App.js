202be4bb8ceb11fdc2a55df1a1c1cb7bc7384376
prateekagashe <prateekagashe@gmail.com>
2025-03-21 23:22:17 +0530

    asad

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
